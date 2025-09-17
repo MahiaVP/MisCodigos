e536eddb71aa7cd3b55cc28e0913e53190ac5030
MahiaVP <sunnix035@gmail.com>
2025-09-17 09:38:01 -0500

    Add files via upload

---
+++ C++/ConditionalIfElseIfelse/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main () {
+	int score = 85;
+	
+	if (score >= 90) {
+		std::cout << "Grade: A\n";
+	} else if (score >= 80) {
+		std::cout << "Grade: B\n";
+	} else if (score >= 70){
+		std::cout << "Grade: C\n";
+	} else {
+		std::cout << "Grade: F\n";
+	}
+	
+	return 0;
+}
