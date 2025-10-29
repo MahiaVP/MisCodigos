9a417d4393df6e51c5912a1a6593863283330c33
MahiaVP <sunnix035@gmail.com>
2025-10-29 15:03:34 -0500

    Update SumaHasta10.cpp

--- C/Taller 2/SumaHasta10.cpp
+++ C/Taller 2/SumaHasta10.cpp
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 int main(){
-	int k,a=1,suma=0;
-	do{
-  suma=0;
-		for(a=1;a<=10;a++){
-			suma=suma+a;
-		}
-		printf("La suma de 1+2+3...10 es:\n%d\n",suma);
-		printf("Desea volver a ejecutar el programa?\n1)Si\n0)No\n");
-		scanf("%d",&k);
-	}while(k==1);
+    int k,a=1,suma=0;
+    
+    do{
+        suma=0;
+        for(a=1;a<=10;a++){
+            suma=suma+a;
+        }
+            printf("La suma de 1+2+3...10 es:\n%d\n",suma);
+            printf("Desea volver a ejecutar el programa?\n1)Si\n0)No\n");
+            scanf("%d",&k);
+    }while(k==1);
 }
